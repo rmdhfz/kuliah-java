f5b4f784f9c09fbc1109944cf411c20671f473c8
Hafiz Ramadhan <hfzrmd@gmail.com>
2021-03-13 22:35:04 +0700

    Done: Latihan3 with Switch, Array, Currency

--- Latihan3.java
+++ Latihan3.java
@@ -1,6 +1,7 @@
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Scanner;
+import java.util.*;
 
 public class Latihan3{
 
@@ -18,7 +19,9 @@ public class Latihan3{
                 int hargaPensil = 2000, hargaBuku   = 3000, hargaPenghapus = 1000, jumlahBarangYangDiBeli;
         	int total = 0, satuan = 0, pilihMenu = 0, uangBayar = 0, uangKurang = 0, sisaKembalian = 0;
         	String loop, barangYangDibeli;
-                
+
+                List<String> carts = new ArrayList<String>();
+
                 do {
                         System.out.println("=================");
                         System.out.println("::- Lapaku -::");
@@ -33,18 +36,18 @@ public class Latihan3{
                         switch (pilihMenu){
                                 case 1:
                                         total = total+hargaPensil;
-                                        barangYangDibeli = "Pensil";
+                                        carts.add("1 Pensil\t -> " + kursIndonesia.format(hargaPensil));
                                         break;
                                 case 2:
                                         total = total+hargaBuku;
-                                        barangYangDibeli = "Buku";
+                                        carts.add("1 Buku\t -> " + kursIndonesia.format(hargaBuku));
                                         break;
                                 case 3:
                                         total = total+hargaPenghapus;
-                                        barangYangDibeli = "Penghapus";
+                                        carts.add("1 Penghapus\t -> " + kursIndonesia.format(hargaPenghapus));
                                         break;
                                 default:
-                                        System.out.println("Pembelian kamu tidak diketahui :(");
+                                        System.out.println("Pembelian tidak diketahui :(");
                                         break;
                         }
                         System.out.print("Kamu mau beli lagi ? (y/n) ");
@@ -52,24 +55,36 @@ public class Latihan3{
                 } while (loop.equals("y") || loop.equals("Y"));
 
                 /*struk area*/
+                System.out.println("----------------");
                 System.out.println("Informasi Belanja");
                 System.out.println("----------------");
                 System.out.println("Total Belanja: " + kursIndonesia.format(total));
                 System.out.println("----------------");
-                System.out.print("Masukan uang kamu:  ");
+                System.out.print("Masukan uang:  ");
 
                 uangBayar = simpan.nextInt();
                 
                 if (uangBayar <= total){
                         uangKurang = total - uangBayar;
-                        System.out.println("Maaf, uang kamu kurang " + kursIndonesia.format(uangKurang));
+                        System.out.println("Maaf, uang anda kurang " + kursIndonesia.format(uangKurang));
                         System.out.println("Masukan uang sebesar " + kursIndonesia.format(uangKurang) + " untuk melanjutkan transaksi.");
                 }else{
                         sisaKembalian = uangBayar - total;
-                        System.out.println("Informasi Struk Lapaku Anda");
-                        System.out.println("---------------------------------");
-                        System.out.println("Selamat Pembayaran Berhasil");
-                        System.out.println("Kembalian: " + kursIndonesia.format(sisaKembalian));
+                        System.out.println("----------------------------------");
+                        System.out.println("Selamat Pembayaran Berhasil!\n");
+                        System.out.println("-----------------------------------------");
+                        System.out.println("|::- Lapaku -::\t\t\t\t|");
+                        System.out.println("|Jl. Raya Ciomas Rahayu\t\t\t|");
+                        System.out.println("----------------------------------------|");
+                        for (String barang : carts) {
+                                System.out.println("|- " + barang + "\t|");
+                        }
+                        System.out.println("|---------------------------------------|");
+                        System.out.println("|Subtotal: \t" + kursIndonesia.format(total) + "\t\t|");
+                        System.out.println("|Total: \t" + kursIndonesia.format(total) + "\t\t|");
+                        System.out.println("|Payment: \t" + kursIndonesia.format(uangBayar) + "\t\t|");
+                        System.out.println("|Change: \t" + kursIndonesia.format(sisaKembalian) + "\t\t|");
+                        System.out.println("|---------------------------------------|");
                 }
         }
 }
