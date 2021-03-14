93ec885750b904ed3e6aee5e1be6da04831fa466
Hafiz Ramadhan <hfzrmd@gmail.com>
2021-03-14 15:33:09 +0700

    Add: For, DoWhile, LampuLaluLintas

---
+++ DoWhile.java
@@ -0,0 +1,12 @@
+public class DoWhile{
+
+	public static void main(String args[]){
+		int h = 0;
+		do {
+			System.out.println("Hallo Hafiz Ramadhan. Ini Do-While ke-" + h);
+			h++;
+		}while(h < 5);
+		
+	}
+
+}
---
+++ For.java
@@ -0,0 +1,10 @@
+public class For{
+
+	public static void main(String args[]){
+		int h;
+		for (h = 0; h < 5; h++) {
+			System.out.println("Hallo Hafiz Ramadhan. Ini For ke-" + h);
+		}
+	}
+
+}
